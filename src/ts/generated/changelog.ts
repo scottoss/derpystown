20ce1ea878494ae738b15a1c848a941aeeac744b
scott <ossscott40@gmail.com>
2021-08-21 22:28:17 +0200

    first commit

---
+++ src/ts/generated/changelog.ts
@@ -0,0 +1,122 @@
+/* tslint:disable */
+
+export const CHANGELOG: { version: string; changes: string[]; }[] = [
+  {
+    "version": "v0.1.8",
+    "changes": [
+      "added new songs",
+      "added scale limit (3)",
+      "fixed and added new sprites",
+      "added hidden easter egg for future update (can you find it?)"
+    ]
+  },
+  {
+    "version": "v0.1.7",
+    "changes": [
+      "added more house items",
+      "fixed cutiemark editor"
+    ]
+  },
+  {
+    "version": "v0.1.6",
+    "changes": [
+      "updated cutiemark editor",
+      "added credits of original game back",
+      "map updated"
+    ]
+  },
+  {
+    "version": "v0.1.5",
+    "changes": [
+      "happy easter",
+      "fixed some bugs with music",
+      "added new flowers"
+    ]
+  },
+  {
+    "version": "v0.1.4",
+    "changes": [
+      "added entry logo and statues on spawn"
+    ]
+  },
+  {
+    "version": "v0.1.3",
+    "changes": [
+      "removed old music to make place for new country music"
+    ]
+  },
+  {
+    "version": "v0.1.2",
+    "changes": [
+      "added bridge on island"
+    ]
+  },
+  {
+    "version": "v0.1.1",
+    "changes": [
+      "added discomap",
+      "added new music to discomap made by chrysalis"
+    ]
+  },
+  {
+    "version": "v0.1.0",
+    "changes": [
+      "fixed public island/house"
+    ]
+  },
+  {
+    "version": "v0.0.9",
+    "changes": [
+      "added muffins"
+    ]
+  },
+  {
+    "version": "v0.0.8",
+    "changes": [
+      "added a office space for admins and team",
+      "here u can go if u have questions"
+    ]
+  },
+  {
+    "version": "v0.0.7",
+    "changes": [
+      "added new music"
+    ]
+  },
+  {
+    "version": "v0.0.6",
+    "changes": [
+      "changed pony-glow color"
+    ]
+  },
+  {
+    "version": "v0.0.5",
+    "changes": [
+      "facebook login fixed"
+    ]
+  },
+  {
+    "version": "v0.0.4",
+    "changes": [
+      "added scaling"
+    ]
+  },
+  {
+    "version": "v0.0.3",
+    "changes": [
+      "added ponyshare"
+    ]
+  },
+  {
+    "version": "v0.0.2",
+    "changes": [
+      "house item limit set to 20000"
+    ]
+  },
+  {
+    "version": "v0.0.1",
+    "changes": [
+      "server started"
+    ]
+  }
+];
---
+++ src/ts/generated/hash.ts
@@ -0,0 +1,2 @@
+export const HASH = 'sQ8QPxJDY0';
+export const STAMP = 765052648;
